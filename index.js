d9e14a405cca0a9e9d83dc93709423f0bb172b51
Davi <dp.junior1997@gmail.com>
2024-11-27 11:06:23 -0300

    feat: add route to create a new task

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).send("Hello world!");
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await prisma.task.findMany();
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const { description, completed } = req.body;
+
+        if (!description || typeof completed !== "boolean") {
+            return res.status(400).send({ error: "Dados inválidos!" });
+        }
+
+        const newTask = await prisma.task.create({
+            data: {
+                description,
+                completed,
+            },
+        });
+
+        res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
 app.listen(8000, () => {

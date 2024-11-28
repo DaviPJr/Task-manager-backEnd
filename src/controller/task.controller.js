8b88ef0f294f80de79f4bbc37920e6f5cf7314e1
Davi <dp.junior1997@gmail.com>
2024-11-28 09:30:59 -0300

    feat: moving all endpoints to controller folder

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -15,6 +15,91 @@ class TaskController {
             this.res.status(500).send({ error: error.message });
         }
     }
+
+    async getTaskById() {
+        const taskId = parseInt(this.req.params.id);
+
+        try {
+            const task = await prisma.task.findUnique({
+                where: {
+                    id: taskId,
+                },
+            });
+            if (!task) {
+                return this.res.status(404).send("Tarefa não encontrada!");
+            }
+            this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send({ error: error.message });
+        }
+    }
+
+    async addTask() {
+        try {
+            const { description, completed } = this.req.body;
+
+            if (!description || typeof completed !== "boolean") {
+                return this.res.status(400).send({ error: "Dados inválidos!" });
+            }
+
+            const newTask = await prisma.task.create({
+                data: {
+                    description,
+                    completed,
+                },
+            });
+
+            this.res.status(201).send(newTask);
+        } catch (error) {
+            this.res.status(500).send({ error: error.message });
+        }
+    }
+
+    async updateTask() {
+        const taskId = parseInt(this.req.params.id);
+        const updates = this.req.body;
+        try {
+            const updatedTask = await prisma.task.update({
+                where: {
+                    id: taskId,
+                },
+                data: updates,
+            });
+
+            this.res.status(200).send(updatedTask);
+        } catch (error) {
+            if (error.code === "P2025") {
+                return this.res
+                    .status(404)
+                    .send({ error: "Tarefa não encontrada" });
+            }
+            this.res.status(500).send({ error: error.message });
+        }
+    }
+
+    async deleteTask() {
+        const taskId = parseInt(this.req.params.id);
+
+        try {
+            if (isNaN(taskId)) {
+                return this.res.status(400).send({ error: "ID inválido" });
+            }
+            const deletedTask = await prisma.task.delete({
+                where: {
+                    id: taskId,
+                },
+            });
+
+            this.res.status(200).send(deletedTask);
+        } catch (error) {
+            if (error.code === "P2025") {
+                return this.res
+                    .status(404)
+                    .send({ error: "Tarefa não encontrada" });
+            }
+            this.res.status(500).send({ error: error.message });
+        }
+    }
 }
 
 export default TaskController;
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -8,84 +8,19 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-    const taskId = parseInt(req.params.id);
-
-    try {
-        const task = await prisma.task.findUnique({
-            where: {
-                id: taskId,
-            },
-        });
-        if (!task) {
-            return res.status(404).send("Tarefa não encontrada!");
-        }
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
+    return new TaskController(req, res).getTaskById();
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const { description, completed } = req.body;
-
-        if (!description || typeof completed !== "boolean") {
-            return res.status(400).send({ error: "Dados inválidos!" });
-        }
-
-        const newTask = await prisma.task.create({
-            data: {
-                description,
-                completed,
-            },
-        });
-
-        res.status(201).send(newTask);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
+    return new TaskController(req, res).addTask();
 });
 
 router.patch("/:id", async (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const updates = req.body;
-    try {
-        const updatedTask = await prisma.task.update({
-            where: {
-                id: taskId,
-            },
-            data: updates,
-        });
-
-        res.status(200).send(updatedTask);
-    } catch (error) {
-        if (error.code === "P2025") {
-            return res.status(404).send({ error: "Tarefa não encontrada" });
-        }
-        res.status(500).send({ error: error.message });
-    }
+    return new TaskController(req, res).updateTask();
 });
 
 router.delete("/:id", async (req, res) => {
-    const taskId = parseInt(req.params.id);
-
-    try {
-        if (isNaN(taskId)) {
-            return res.status(400).send({ error: "ID inválido" });
-        }
-        const deletedTask = await prisma.task.delete({
-            where: {
-                id: taskId,
-            },
-        });
-
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        if (error.code === "P2025") {
-            return res.status(404).send({ error: "Tarefa não encontrada" });
-        }
-        res.status(500).send({ error: error.message });
-    }
+    return new TaskController(req, res).deleteTask();
 });
 
 export default router;

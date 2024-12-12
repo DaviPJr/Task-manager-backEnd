ca91945ab1d0fe336d2b30351c5defc6c35755d2
Davi <dp.junior1997@gmail.com>
2024-12-12 12:28:19 -0300

    feat: adding cors to allow front-back communication

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./src/routes/task.routes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/tasks", router);
 
